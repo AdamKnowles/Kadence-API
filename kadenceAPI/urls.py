ebfcc7911ed8d93da10f847c4e09acec296494fe
Adam Knowles <acknowles524@gmail.com>
2019-12-02 16:07:19 -0600

    added view, model, and fixtures for Run instance

--- kadenceAPI/urls.py
+++ kadenceAPI/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url, include
 from rest_framework import routers
 from rest_framework.authtoken.views import obtain_auth_token
+from kadence.models import *
+from kadence.views import Runs
 
 router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'runs', Runs, 'runs')
 
 urlpatterns = [
     url(r'^', include(router.urls)),
